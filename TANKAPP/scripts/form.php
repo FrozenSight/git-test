b93c35f7055d7cdd2cbfd38c79b98c912e5d74cf
FrozenSight <mark.zayakin@yandex.ru>
2018-04-04 19:44:58 +0700

    переместил в папку script

---
+++ TANKAPP/scripts/form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php 
+require 'connect.php';
+$first_name = trim($_REQUEST['first_name']);
+$last_name = trim($_REQUEST['last_name']);
+$email = trim($_REQUEST['email']);
+$facebook = trim($_REQUEST['facebook']);
+
+$position = true;
+if(preg_match("/^http:\/\/www.facebook.com\//", $facebook))
+{$position = false;}
+else if(preg_match("/^www.facebook.com\//", $facebook))
+{$position = false;
+$facebook = "http://" . $facebook;}
+else if($position)
+{$facebook = "http://www.facebook.com/" . $facebook;}
+
+
+$insert_sql = "INSERT INTO users (first_name, last_name, email, facebook)" .
+"VALUES('{$first_name}', '{$last_name}', '{$email}', '{$facebook}');";
+mysql_query($insert_sql);
+echo "<p>Новая запись вставлена в базу!</p>";
+?>
+
+</body>
+</html>
